a1745b66ffb19030cc9b35464d6af106e393a687
Muhammad Taimour <muhammad.taimour95@gmail.com>
2019-12-30 18:34:34 +0500

    Set up app for heroku

--- server/app.js
+++ server/app.js
@@ -2,16 +2,23 @@ const express = require('express');
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
-const path = require("path");
-const cors = require('cors')
+const path = require('path');
+const cors = require('cors');
 
 const { HTTPS_PORT } = require('./config.json');
 
-const privateKey = fs.readFileSync(path.resolve(__dirname, "../sslcert/server.key"), 'utf8');
-const certificate = fs.readFileSync(path.resolve(__dirname, "../sslcert/server.cert"), 'utf8');
+const privateKey = fs.readFileSync(
+  path.resolve(__dirname, '../sslcert/server.key'),
+  'utf8'
+);
+const certificate = fs.readFileSync(
+  path.resolve(__dirname, '../sslcert/server.cert'),
+  'utf8'
+);
 const credentials = { key: privateKey, cert: certificate };
 
 const app = express();
+const port = process.env.PORT || 17000;
 
 //TODO: use for whitelist only
 app.use(cors());
@@ -26,7 +33,7 @@ app.use('/cli', cli);
 const httpsServer = https.createServer(credentials, app);
 
 // httpServer.listen(HTTP_PORT);
-httpsServer.listen(HTTPS_PORT);
-console.log(`server listening at port ${HTTPS_PORT}`);
+app.listen(port);
+console.log(`server listening at port ${port}`);
 
 module.exports = { app };
